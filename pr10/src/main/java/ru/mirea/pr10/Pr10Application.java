41e4a9a0e85663b124bd78159cd8d6202c88283a
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2021-04-18 11:33:50 +0300

    pr10 init

---
+++ pr10/src/main/java/ru/mirea/pr10/Pr10Application.java
@@ -0,0 +1,15 @@
+package ru.mirea.pr10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr10Application.class, args);
+
+		
+	}
+
+}
